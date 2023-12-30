c57aaab98ec3ac24a78597a39add796c103d82c9
zaeem9233 <zaeemali91@gmail.com>
2023-12-30 14:05:28 +0500

    Project Initialized and Connection established with Database

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
