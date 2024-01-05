5a5218be51e32ab745914de6cbff956b84484420
zaeem9233 <zaeemali91@gmail.com>
2024-01-05 14:53:22 +0500

    API Authentication implemented using sanctum

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+// use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -18,7 +18,7 @@ class AuthController extends Controller
         if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])){
             $user = Auth::user();
 
-            $success = $user->createToken('MyApp')->plianTextToken;
+            $success = $user->createToken('API Authentication')->plainTextToken;
 
             return response(['token' => $success], 200);
             // return $user;
--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('auth/login', [AuthController::class, 'login'])->name('api.login');
 
